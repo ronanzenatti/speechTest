7aaf312f996c77f9e2f50e2d5c254aac45464ebf
Ronan Adriel Zenatti <ronanzenatti@gmail.com>
2020-01-27 21:07:15 -0300

    Reconhecimento de Fala funcionando.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,8 +18,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SpeechRecognition
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
+import { Platform } from '@ionic/angular';
+import { Component, ChangeDetectorRef } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public isRecording = false;
+  public matchs: String[];
+
+  constructor(private plt: Platform, private speechRecognition: SpeechRecognition, private changeDetector: ChangeDetectorRef) { }
+
+  public isIos() {
+    return this.plt.is('ios');
+  }
+
+  public getPermission() {
+    this.speechRecognition.hasPermission()
+      .then((hasPermission: boolean) => {
+        if (!hasPermission) {
+          this.speechRecognition.requestPermission();
+        }
+      });
+  }
+
+  public startListening() {
+    let options = {
+      language: 'pt-BR'
+    }
+
+    this.speechRecognition.startListening(options).subscribe(matchs => {
+      this.matchs = matchs;
+      this.changeDetector.detectChanges();
+    });
+
+    this.isRecording = true;
+
+  }
+
+  public stopListening() {
+    this.speechRecognition.stopListening().then(() => {
+      this.isRecording = false;
+    })
+  }
+
+
 
 }
